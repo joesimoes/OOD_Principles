0d4ef9754c8931d6e09d319e3f744535513c9e38
Joe Simoes <joe@generalassemb.ly>
2014-04-19 10:51:23 -0400

    Add tests

---
+++ lib/hero.rb
@@ -0,0 +1,14 @@
+class Hero
+  attr_reader :strength, :health, :dicepool
+
+  def initialize(attr={})
+    @strength = attr.fetch(:strength, 3)
+    @health = attr.fetch(:health, 10)
+
+    @dicepool = attr.fetch(:dicepool)
+  end
+
+  def attack(monster)
+    @dicepool.skill_check(strength) >= monster.toughness
+  end
+end
---
+++ spec/lib/hero_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+require_relative '../../lib/hero'
+
+describe Hero do 
+  let(:dicepool) { double("dicepool") }
+
+  describe "default attributes" do 
+    let(:hero) { Hero.new dicepool: dicepool }
+
+    it "has default strength equal to 3" do
+      expect(hero.strength).to eq(3)
+    end
+
+    it "has default health equal to 10" do
+      expect(hero.health).to eq(10)
+    end
+  end
+
+  it "can be initialized with custom strength" do
+    hero = Hero.new strength: 3, dicepool: dicepool
+    expect(hero.strength).to eq(3)
+  end
+
+  it "can be initialized with custom health" do 
+    hero = Hero.new health: 8, dicepool: dicepool 
+    expect(hero.health).to eq(8)
+  end
+
+  describe "attack" do
+    it "succeeds" do
+      dicepool.stub(:skill_check).and_return(3)
+      hero = Hero.new dicepool: dicepool
+      monster = double("monster", toughness: 2)
+
+      expect(hero.attack(monster)).to be_true
+    end
+    it "fails" do 
+      dicepool.stub(:skill_check).and_return(1)
+      hero = Hero.new dicepool: dicepool
+      monster = double("monster", toughness: 2)
+
+      expect(hero.attack(monster)).to be_false
+    end
+  end
+end
